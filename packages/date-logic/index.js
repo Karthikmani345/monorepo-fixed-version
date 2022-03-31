3750a346a4a288445ee9d662f871764c2895df12
kartik <kartik@kartiks-MacBook-Pro.local>
2022-03-31 18:10:33 +0530

    feat: added permission module

---
+++ packages/date-logic/index.js
@@ -0,0 +1,18 @@
+module.exports = function getDate(
+  locale = 'en-US',
+  options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+) {
+  return new Date().toLocaleDateString(locale, options);
+};
+
+
+function name() {
+  console.log('name');
+  
+}
+
